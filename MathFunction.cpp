dff68f7aeb833764c30d0fbfb8d71bebfe79dccf
Torsten Gellert <t.gellert@gmail.com>
2012-05-06 13:46:40 +0200

    Class for math. functions added. Missing: brakets and functions and tests

---
+++ MathFunction.cpp
@@ -0,0 +1,19 @@
+#include "MathFunction.h"
+
+int main(int args, char **argv ){
+
+	std::string term{"1+2*3-x"};
+	double x = 1.4;
+	if(args>1)
+		term = std::string(argv[1]);
+	if(args>2)
+		x = boost::lexical_cast<double>(argv[2]);
+
+	MathFunction<double> f{term};
+
+	std::cout<<f.evaluate(x)<<std::endl;
+
+}
+
+
+
